dd2b6e1a7a859c569c202987dcebc9516f974ee2
darkopet <darkodepetrovski@gmail.com>
2022-06-17 14:59:37 +0200

    Intro

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// LARAVEL Welcome
+Route::get('/laravel', function () {
+    return view('laravel');
+});
+
+Route::get('/', function (){
+    return view('intro');
+});
+
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/contact', function(){
+    return view('contact');
 });
