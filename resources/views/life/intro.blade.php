aa7aa8e4caeadd0f5b98d1189b1949ab6709eb5a
darkopet <darkodepetrovski@gmail.com>
2022-06-26 14:10:50 +0200

    Index Changes and Contacts DB

--- resources/views/life/intro.blade.php
+++ resources/views/life/intro.blade.php
@@ -460,9 +460,7 @@
         <div class="max-w-4xl mx-auto sm:px-4 lg:px-8">
             <h1>Welcome at Darko's Home</h1>
 
-            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                <div class="grid grid-cols-1 md:grid-cols-2">
-                    <div class="p-6">
+                                <div class="p-6">
                         <div class="flex items-center">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
                                 <path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
@@ -482,17 +480,19 @@
                             <span class="ml-4">
                                 <a href="/professional" class="underline text-white-900 dark:text-white text-lg btnStack">Professional</a>
                             </span>
-
+                            <span class="ml-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
+                                    <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
+                                    </path>
+                                </svg>
+                            </span>    
                             <!-- <x-button class="ml-4 underline text-white-900 dark:text-white text-lg btnStackml-4 btnStack">Travel</x-button> -->
-
-                            <img src="https://avatars.githubusercontent.com/u/20263411?v=4" alt="Darko" class="center absolute"></img>
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
-                                </path>
-                            </svg>
                         </div>
                     </div>
 
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                    <img src="https://avatars.githubusercontent.com/u/20263411?v=4" class="center absolute"></img>
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                         <div class="flex items-center">
                             <svg fill="none" 
