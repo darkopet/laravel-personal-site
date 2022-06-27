40fa3d7d3e95c87aa50a25308f9eb197944336cb
darkopet <darkodepetrovski@gmail.com>
2022-06-27 13:00:02 +0200

    Contact Form

--- resources/views/life/contact.blade.php
+++ resources/views/life/contact.blade.php
@@ -15,72 +15,69 @@
 
     
     <div class="row">
-
         <!--Grid column-->
-        <div class="col-md-9 mb-md-0 mb-5">
-            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
+        <!-- <div class="col-md-9 mb-md-0 mb-5">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST"> -->
                 <!--Grid row-->
-                <div class="row">
+                <!-- <div class="row"> -->
 
                     <!--Grid column-->
-                    <div class="col-md-6">
+                    <!-- <div class="col-md-6">
                         <div class="md-form mb-0">
                             <input type="text" id="name" name="name" class="form-control">
                             <label for="name" class="">Your name</label>
                         </div>
-                    </div>
+                    </div> -->
                     <!--Grid column-->
 
                     <!--Grid column-->
-                    <div class="col-md-6">
+                    <!-- <div class="col-md-6">
                         <div class="md-form mb-0">
                             <input type="text" id="email" name="email" class="form-control">
                             <label for="email" class="">Your email</label>
                         </div>
-                    </div>
+                    </div> -->
                     <!--Grid column-->
 
-                </div>
+                <!-- </div> -->
                 <!--Grid row-->
 
                 <!--Grid row-->
-                <div class="row">
+                <!-- <div class="row">
                     <div class="col-md-12">
                         <div class="md-form mb-0">
                             <input type="text" id="subject" name="subject" class="form-control">
                             <label for="subject" class="">Subject</label>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <!--Grid row-->
 
                 <!--Grid row-->
-                <div class="row">
-
+                <!-- <div class="row"> -->
                     <!--Grid column-->
-                    <div class="col-md-12">
-
+                    <!-- <div class="col-md-12">
                         <div class="md-form">
-                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            <textarea type="text" id="message" name="message" rows="2" 
+                                      class="form-control md-textarea">
+                            </textarea>
                             <label for="message">Your message</label>
                         </div>
-
                     </div>
-                </div>
-                <!--Grid row-->
 
-            </form>
+                </div> -->
+                <!--Grid row-->
+            <!-- </form>
 
             <div class="text-center text-md-left">
                 <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
             </div>
             <div class="status"></div>
-        </div>
+        </div> -->
         <!--Grid column-->
 
         <!--Grid column-->
-        <div class="col-md-3 text-center">
+        <!-- <div class="col-md-3 text-center">
             <ul class="list-unstyled mb-0">
                 <li><i class="fas fa-map-marker-alt fa-2x"></i>
                     <p>San Francisco, CA 94126, USA</p>
@@ -94,10 +91,21 @@
                     <p>contact@mdbootstrap.com</p>
                 </li>
             </ul>
-        </div>
+        </div> -->
         <!--Grid column-->
 
     </div>
 
 </section>
 <!--Section: Contact v.2-->
+
+<x-layout>
+        <form method="POST" action="/life" enctype="multipart/form-data">
+            @csrf  
+                <x-form.input name="Your name" required/>
+                <x-form.input name="Your email" required/>
+                <x-form.input name="Subject" type="sentence" required/>
+                <x-form.textarea name="Message" required/>
+                <x-form.button>Send</x-form.button>
+        </form>
+</x-layout>
