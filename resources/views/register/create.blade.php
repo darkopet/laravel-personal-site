1dbdb13957485143ed4e708ee54211a5fcd3ad36
darkopet <darkodepetrovski@gmail.com>
2022-06-30 13:00:38 +0200

    RegSess format

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -6,11 +6,11 @@
 
                 <form method="POST" action="/register" class="mt-10">
                     @csrf
-                        <x-form.input name="name" required/>
-                        <x-form.input name="username" required/>
-                        <x-form.input name="email" type="email" required/>
-                        <x-form.input name="password" type="password" autocomplete="new-password" required/>
-                        <x-form.button>Sign Up</x-form.button>
+                    <x-form.input name="name" required />
+                    <x-form.input name="username" required />
+                    <x-form.input name="email" type="email" required />
+                    <x-form.input name="password" type="password" autocomplete="new-password" required />
+                    <x-form.button>Sign Up</x-form.button>
                 </form>
             </x-panel>
         </main>
