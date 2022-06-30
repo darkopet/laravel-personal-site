629a75ef3d5cacfd12228905e12e385ce3fdae01
darkopet <darkodepetrovski@gmail.com>
2022-06-30 12:54:58 +0200

    contacts format

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,6 +1,5 @@
-<a href="/" 
-   class="underline text-gray-900 dark:text-white">
-      <button>Darko's Home</button>
+<a href="/" class="underline text-gray-900 dark:text-white">
+    <button>Darko's Home</button>
 </a>
 
 <!--Section: Contact v.2-->
@@ -15,45 +14,45 @@
 
     <x-layout>
         <form method="POST" action="/contact" enctype="multipart/form-data">
-            @csrf  
-                <x-form.input name="Your name" required/>
-                <x-form.input name="Your email" required/>
-                <x-form.input name="Subject" type="sentence" required/>
-                <x-form.textarea name="Message" required/>
-                <x-form.button>Send</x-form.button>
+            @csrf
+            <x-form.input name="Your name" required />
+            <x-form.input name="Your email" required />
+            <x-form.input name="Subject" type="sentence" required />
+            <x-form.textarea name="Message" required />
+            <x-form.button>Send</x-form.button>
         </form>
     </x-layout>
 
     <!-- <div class="row"> -->
-        <!--Grid column-->
-        <!-- <div class="col-md-9 mb-md-0 mb-5">
+    <!--Grid column-->
+    <!-- <div class="col-md-9 mb-md-0 mb-5">
             <form id="contact-form" name="contact-form" action="mail.php" method="POST"> -->
-                <!--Grid row-->
-                <!-- <div class="row"> -->
+    <!--Grid row-->
+    <!-- <div class="row"> -->
 
-                    <!--Grid column-->
-                    <!-- <div class="col-md-6">
+    <!--Grid column-->
+    <!-- <div class="col-md-6">
                         <div class="md-form mb-0">
                             <input type="text" id="name" name="name" class="form-control">
                             <label for="name" class="">Your name</label>
                         </div>
                     </div> -->
-                    <!--Grid column-->
+    <!--Grid column-->
 
-                    <!--Grid column-->
-                    <!-- <div class="col-md-6">
+    <!--Grid column-->
+    <!-- <div class="col-md-6">
                         <div class="md-form mb-0">
                             <input type="text" id="email" name="email" class="form-control">
                             <label for="email" class="">Your email</label>
                         </div>
                     </div> -->
-                    <!--Grid column-->
+    <!--Grid column-->
 
-                <!-- </div> -->
-                <!--Grid row-->
+    <!-- </div> -->
+    <!--Grid row-->
 
-                <!--Grid row-->
-                <!-- <div class="row">
+    <!--Grid row-->
+    <!-- <div class="row">
                     <div class="col-md-12">
                         <div class="md-form mb-0">
                             <input type="text" id="subject" name="subject" class="form-control">
@@ -61,12 +60,12 @@
                         </div>
                     </div>
                 </div> -->
-                <!--Grid row-->
+    <!--Grid row-->
 
-                <!--Grid row-->
-                <!-- <div class="row"> -->
-                    <!--Grid column-->
-                    <!-- <div class="col-md-12">
+    <!--Grid row-->
+    <!-- <div class="row"> -->
+    <!--Grid column-->
+    <!-- <div class="col-md-12">
                         <div class="md-form">
                             <textarea type="text" id="message" name="message" rows="2" 
                                       class="form-control md-textarea">
@@ -76,18 +75,18 @@
                     </div>
 
                 </div> -->
-                <!--Grid row-->
-            <!-- </form>
+    <!--Grid row-->
+    <!-- </form>
 
             <div class="text-center text-md-left">
                 <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
             </div>
             <div class="status"></div>
         </div> -->
-        <!--Grid column-->
+    <!--Grid column-->
 
-        <!--Grid column-->
-        <!-- <div class="col-md-3 text-center">
+    <!--Grid column-->
+    <!-- <div class="col-md-3 text-center">
             <ul class="list-unstyled mb-0">
                 <li><i class="fas fa-map-marker-alt fa-2x"></i>
                     <p>San Francisco, CA 94126, USA</p>
@@ -102,7 +101,7 @@
                 </li>
             </ul>
         </div> -->
-        <!--Grid column-->
+    <!--Grid column-->
     <!-- </div>
 </section> -->
-<!--Section: Contact v.2-->
+    <!--Section: Contact v.2-->
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -12,6 +12,7 @@
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                             <tbody class="bg-white divide-y divide-gray-200">
@@ -30,14 +31,14 @@
                                     <!-- <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                         <a href="/contacts/{{ $contact->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
                                     </td> -->
-                                        <form method="POST" action="/contact" enctype="multipart/form-data">
-                                            @csrf
-                                            <x-form.input name="Your name" required />
-                                            <x-form.input name="Your email" required />
-                                            <x-form.input name="Subject" type="sentence" required />
-                                            <x-form.textarea name="Message" required />
-                                            <x-form.button>Send</x-form.button>
-                                        </form>
+                                    <form method="POST" action="/contact" enctype="multipart/form-data">
+                                        @csrf
+                                        <x-form.input name="Your name" required />
+                                        <x-form.input name="Your email" required />
+                                        <x-form.input name="Subject" type="sentence" required />
+                                        <x-form.textarea name="Message" required />
+                                        <x-form.button>Send</x-form.button>
+                                    </form>
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                         <form method="POST" action="/contacts/{{ $contact->id }}">
                                             @csrf
@@ -51,6 +52,7 @@
                             </tbody>
                         </table>
                     </div>
+                    
                 </div>
             </div>
         </div>
